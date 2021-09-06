c056eacf6e2902870c9afb51d9d312522789b3f1
Polyca <polyca123@gmail.com>
2021-09-06 14:13:19 -0700

    app.js javascript have been added

--- app.js
+++ app.js
@@ -1,35 +1,112 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require("./lib/Manager.js")
+const Engineer = require("./lib/Engineerjs")
+const Intern = require("./lib/Internjs")
+const { prompt } = require("inquirer")
+const path = require("path")
+const { writeFile } = require("fs")
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html")
 
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRendererjs")
 
+const team = []
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const makeManager = ({ name, id, email }) => {
+  prompt({
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is their office number?'
+  })
+    .then(({ officeNumber }) => {
+      team.push(new Manager(name, id, email, officeNumber))
+      menu()
+    })
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const makeEngineer = ({ name, id, email }) => {
+  prompt({
+    type: 'input',
+    name: 'GitHub Username',
+    message: 'What is their GitHub Username?'
+  })
+    .then(({ gitHub }) => {
+      team.push(new Engineer(name, id, email, gitHub))
+      menu()
+    })
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const makeIntern = ({ name, id, email }) => {
+  prompt({
+    type: 'input',
+    name: 'School',
+    message: 'What school do they go to?'
+  })
+    .then(({ school }) => {
+      team.push(new Intern(name, id, email, school))
+      menu()
+    })
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+const makeEmployee = () => {
+  prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: 'Who would you like to create?',
+      choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is their name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is their ID?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is their email?'
+    }
+  ])
+    .then(({ role, ...employee }) => {
+      switch (role) {
+        case 'Manager':
+          makeManager(employee)
+          break
+        case 'Engineer':
+          makeEngineer(employee)
+          break
+        case 'Intern':
+          makeIntern(employee)
+          break
+      }
+    })
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const menu = () => {
+  prompt({
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to do?',
+    choices: ['Create new employee', 'Done']
+  })
+    .then(({ action }) => {
+      switch (action) {
+        case 'Create new employee':
+          makeEmployee()
+          break
+        case 'Done':
+          writeFile('./output/team.html', render(team), err => {
+            if (err) { console.log(err) }
+            console.log('Team have been built!')
+          })
+          break
+      }
+    })
+}
+
+menu()
